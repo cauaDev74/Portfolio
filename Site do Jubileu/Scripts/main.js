f4dcac579b6cbe37f2955bd1d2fdac4530906b59
Cauã <barretocaua74@gmail.com>
2025-08-04 00:39:06 -0300

    Pasta de imagens adicionada

--- Site do Jubileu/Scripts/main.js
+++ Site do Jubileu/Scripts/main.js
@@ -1,24 +1,26 @@
-const botaoL = document.getElementById("botao");
-botaoL.addEventListener("submit", function logar(){
+const formularioJubileu = document.getElementById("cadastro");
+formularioJubileu.addEventListener("submit", function logar(event){
+    event.preventDefault();
     const nome = document.getElementById("nome").value;
     const idade = document.getElementById("idade").value;
     const senha = document.getElementById("senha").value;
 
     if(nome.lenght < 3){
-        alert("Nome deve possuir mais de 3 caracteres");
+        alert("Nome deve possuir mais de 3 letras.");
         return;
     }
-    if(idade < 18 || idade > 100){
-        alert("Idade deve ser maior que 17 e menor que 100");
+    
+    if(idade < 18 || idade > 120){
+        alert("Idade deve estar entre 18 e 120 anos.");
         return;
     }
+
     if(senha.lenght < 6){
-        alert('Senha inválida.');
+        alert("Senha deve possuir mais de 6 letras.");
         return;
     }
 
     const concluido = document.getElementById("result");
-    concluido.innerText(`Bem vindo, ${nome}.`);
-    console.log("Tá funfando");
-})
-
+    concluido.innerHTML = `Bem vindo, ${nome}`;
+    console.log('Tá a funcionar.');
+});
